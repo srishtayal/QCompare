5f43bd497fefbb08766d3c09de643f5793acef66
Srishti Tayal <168971159+srishtayal@users.noreply.github.com>
2025-06-15 15:58:04 +0530

    update port

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL = 'http://localhost:3000';
 
 export const fetchComparison = async (query, pincode) => {
     const res = await axios.post(`${BASE_URL}/search/compare`, { query, pincode });

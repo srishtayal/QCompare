746b2640043f9b1ae27cf40885b9be798397f6c0
Srishti Tayal <168971159+srishtayal@users.noreply.github.com>
2025-06-16 21:20:18 +0530

    add photo in zepto

--- backend/scrapers/zepto.js
+++ backend/scrapers/zepto.js
@@ -54,21 +54,6 @@ async function fetchZeptoPrices(query, pincode = "110078") {
     const productCard = "[data-testid='product-card']";
     await page.waitForSelector(productCard, { timeout: 30000 });
 
-    // const products = await page.$$eval("[data-testid='product-card']", cards => {
-    //   return cards.map(card => {
-    //     const getText = (selector) => card.querySelector(selector)?.innerText.trim() || "";
-    //     const getPrice = (selector) => card.querySelector(selector)?.innerText.match(/₹\d+/)?.[0] || "";
-
-    //     return {
-    //       name: getText("[data-testid='product-card-name']"),
-    //       quantity: getText("[data-testid='product-card-quantity']"),
-    //       price: getPrice("[data-testid='product-card-price']"),
-    //       originalPrice: getPrice("p.line-through"),
-    //       deliveryTime: getText("span.font-extrabold").replace(/\s+/g, " "),
-    //     };
-    //   });
-    // });
-
     const products = await page.$$eval("[data-testid='product-card']", cards => {
       return cards.map(card => {
         const getText = (selector) => card.querySelector(selector)?.innerText.trim() || "";

a3deba88fd595bf75d799b78f3a52e73e7876b1a
Aditya Raj Singh <aditya.r.singh0607@gmail.com>
2025-06-20 14:41:59 +0530

    final ui changes- responsive

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,16 +1,16 @@
 export default function SearchBar({ query, setQuery, onSearch }) {
   return (
-    <div className="flex items-center space-x-3">
+    <div className="flex flex-col sm:flex-row items-center sm:space-x-4 space-y-3 sm:space-y-0 w-full justify-evenly">
       <input
         type="text"
         placeholder="Search for products like 'ice cream'"
-        className="w-96 px-4 py-2 ml-8 mr-16 rounded-3xl border border-gray-600 bg-[#0f1117] text-white placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 transition-all duration-200 "
+        className="w-xs md:w-full sm:max-w-lg px-4 py-2 rounded-3xl border border-gray-600 bg-[#0f1117] text-white placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 transition-all duration-200"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         onKeyDown={(e) => e.key === "Enter" && onSearch()}
       />
       <button
-        className="px-6 py-2 rounded-full border border-white/20 text-white font-semibold hover:bg-white/10 backdrop-blur-md transition-all duration-200 cursor-pointer"
+        className="w-auto lg:w-auto md:w-auto sm:w-auto px-6 py-2 rounded-full border border-white/20 text-white font-semibold hover:bg-white/10 backdrop-blur-md transition-all duration-200 cursor-pointer"
         onClick={onSearch}
       >
         Compare

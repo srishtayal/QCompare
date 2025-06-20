536a11c6349bab29b98df07ca8d82a2a074055da
Aditya Raj Singh <aditya.r.singh0607@gmail.com>
2025-06-20 14:54:36 +0530

    minor bug

--- frontend/src/components/LocationSelector.jsx
+++ frontend/src/components/LocationSelector.jsx
@@ -4,7 +4,7 @@ export default function LocationSelector({ setPincode }) {
   } ;
 
   return (
-    <div className="mr-0 ml-6">
+    <div className="mr-0 md:ml-6">
       <input
         className="w-xs md:w-xs px-4 py-2  rounded-3xl border border-gray-600 bg-[#0f1117] text-white placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 transition-all duration-200 "
         type="text"
